9017f024448b040a2828808758e6d658dee9b540
Juris Daksa <juris.daksa@outlook.com>
2025-02-03 11:37:54 +0200

    Added db schema

---
+++ dbImport/db_schema.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE food_prices_db;
+
+\c food_prices_db;
+
+CREATE TABLE IF NOT EXISTS stores (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id UUID PRIMARY KEY,
+    category_id INT,
+    store_id INT,
+    title VARCHAR(255) NOT NULL,
+    slug VARCHAR(255),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (store_id) REFERENCES stores(id)
+);
+
+CREATE TABLE IF NOT EXISTS current_prices (
+    product_id UUID PRIMARY KEY,
+    price NUMERIC(10, 2),
+    retail_price NUMERIC(10, 2),
+    discount INT,
+    comparable_price NUMERIC(10, 2),
+    unit VARCHAR(50),
+    date_updated DATE,
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE IF NOT EXISTS historical_prices (
+    product_id UUID,
+    date DATE,
+    price NUMERIC(10, 2),
+    retail_price NUMERIC(10, 2),
+    discount INT,
+    comparable_price NUMERIC(10, 2),
+    unit VARCHAR(50),
+    PRIMARY KEY (product_id, date),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE IF NOT EXISTS pdn_ids (
+    product_id UUID PRIMARY KEY,
+    matched_id INT,
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
