e669cd1a3f91d74f6c3982d9117f347be9dca8e7
Juris Daksa <juris.daksa@outlook.com>
2025-02-10 17:05:17 +0200

    Added loyalty prices to barbora

--- stores/barbora/scraper.js
+++ stores/barbora/scraper.js
@@ -1,59 +1,81 @@
 export async function extractProducts(page, baseUrl) {
-  let products = [];
-  let currentPage = 1;
-  let hasNextPage = true;
+    let products = [];
+    
+    try {
+        const newProducts = await page.evaluate(() => {
+            const items = [];
+            const productElements = document.querySelectorAll('[id^="fti-product-card-category-page-"]');
 
-  while (hasNextPage) {
-      const newProducts = await page.evaluate(() => {
-          const items = [];
-          const productElements = document.querySelectorAll('[id^="fti-product-card-category-page-"]');
+            productElements.forEach(product => {
+                const data = product.getAttribute('data-b-for-cart');
+                if (data) {
+                    const productInfo = JSON.parse(data);
+                    const title = productInfo.title;
 
-          productElements.forEach(product => {
-              const data = product.getAttribute('data-b-for-cart');
-              if (data) {
-                  const productInfo = JSON.parse(data);
-                  const title = productInfo.title;
-                  const price = productInfo.price;
-                  const retailPrice = productInfo.retail_price;
-                  const discount = productInfo.promotion ? productInfo.promotion.percentage : null;
-                  const productUrl = productInfo.Url;
-                  const unitPrice = productInfo.comparative_unit_price;
-                  const unit = productInfo.comparative_unit;
-                  items.push({ title, price, retailPrice, discount, productUrl, unitPrice, unit });
-              }
-          });
+                    const retailPrice = productInfo.retail_price || productInfo.price;
+                    let retailUnitPrice = productInfo.comparative_unit_price ? Math.round(parseFloat(productInfo.comparative_unit_price) * 100) / 100 : null;
 
-          return items;
-      });
+                    let discountPriceAmount = null;
+                    let discountUnitPrice = null;
+                    let discount = null;
+                    let loyaltyDiscount = null;
 
-      products = products.concat(newProducts);
+                    if (productInfo.promotion) {
+                        if (productInfo.promotion.type === 'DISCOUNT_PRICE') {
+                            discount = productInfo.promotion.percentage;
+                            discountPriceAmount = Math.round(productInfo.price * 100) / 100;
+                            discountUnitPrice = productInfo.comparative_unit_price ? Math.round(parseFloat(productInfo.comparative_unit_price) * 100) / 100 : null;
+                            retailUnitPrice = productInfo.promotion.oldComparativeRate ? Math.round(parseFloat(productInfo.promotion.oldComparativeRate) * 100) / 100 : retailUnitPrice;
+                        } else if (productInfo.promotion.type === 'LOYALTY_PRICE') {
+                            loyaltyDiscount = productInfo.promotion.percentage;
+                        }
+                    }
 
-      const nextPageLink = await page.evaluate(() => {
-          const elements = Array.from(document.querySelectorAll('a'));
-          const nextPageElement = elements.find(element => element.textContent.includes('»'));
-          return nextPageElement ? nextPageElement.href : null;
-      });
+                    const productUrl = `https://barbora.lv/produkti/${productInfo.Url}`;
+                    const unit = productInfo.comparative_unit;
 
-      console.log(`Next page link: ${nextPageLink}`);
+                    items.push({
+                        title,
+                        unit: unit ? unit.trim() : '',
+                        retailPrice: {
+                            amount: retailPrice,
+                            unitPrice: retailUnitPrice
+                        },
+                        discountPrice: {
+                            amount: discountPriceAmount,
+                            unitPrice: discountUnitPrice,
+                            discount: discount
+                        },
+                        loyaltyPrice: {
+                            amount: loyaltyDiscount ? Math.round(productInfo.price * 100) / 100 : null,
+                            unitPrice: loyaltyDiscount ? discountUnitPrice : null,
+                            discount: loyaltyDiscount
+                        },
+                        productUrl
+                    });
+                }
+            });
 
-      if (nextPageLink) {
-          const absoluteNextPageLink = new URL(nextPageLink, baseUrl).href;
-          const nextPageNumber = new URL(absoluteNextPageLink).searchParams.get('page');
-          console.log(`Next page URL: ${absoluteNextPageLink}`);
-          console.log(`Next page number: ${nextPageNumber}`);
-          if (nextPageNumber && parseInt(nextPageNumber) > currentPage) {
-              currentPage = parseInt(nextPageNumber);
-              await Promise.all([
-                  page.waitForNavigation(),
-                  page.goto(`${absoluteNextPageLink}`, { waitUntil: "domcontentloaded", timeout: 60000 })
-              ]);
-          } else {
-              hasNextPage = false;
-          }
-      } else {
-          hasNextPage = false;
-      }
-  }
+            return items;
+        });
 
-  return products;
+        products = products.concat(newProducts);
+    } catch (error) {
+        throw new Error(`Error extracting products on page ${page.url()}: ${error.message}`);
+    }
+
+    return products;
+}
+
+export async function getNextPageLink(page) {
+    try {
+        const nextPageLink = await page.evaluate(() => {
+            const elements = Array.from(document.querySelectorAll('a'));
+            const nextPageElement = elements.find(element => element.textContent.includes('»'));
+            return nextPageElement ? nextPageElement.href : null;
+        });
+        return nextPageLink;
+    } catch (error) {
+        throw new Error(`Error getting next page link on page ${page.url()}: ${error.message}`);
+    }
 }
